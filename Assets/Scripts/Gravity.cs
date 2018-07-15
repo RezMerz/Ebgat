dad1189c531850ee3b3d87bf1ad7378045307075
Noise <rmirzaee95@gmail.com>
2018-07-16 03:40:25 +0430

    Gravity Fixed

--- Assets/Scripts/Gravity.cs
+++ Assets/Scripts/Gravity.cs
@@ -74,9 +74,7 @@ public class Gravity : MonoBehaviour {
                 // Go to on Ground state
                 if(hit)
                 {
-                   // print(hitObjects[0].distance);
-                    transform.position += new Vector3(0,(hitObjects[0].distance - charStats.size.y / 2));
-                   // print(hitObjects[0].distance - charStats.size.y / 2);
+                    transform.position -= new Vector3(0,(hitObjects[0].distance - charStats.size.y / 2));
                     charStats.FeetState = EFeetState.Onground;
                     /// Reset Gravity Stats
                 }
