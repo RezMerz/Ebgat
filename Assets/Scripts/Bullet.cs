0d524e6f090f2f1bf75a2cdce3f952125a8ac524
Yashar Fallah <Yashar.fallah1372@gmail.com>
2018-07-16 22:17:25 +0430

    bullet working

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,17 +6,33 @@ public class Bullet : MonoBehaviour {
     public float damage;
     public float range;
     public float width;
-    public float gravitySpeed;
+    public float gravitySpeedBase;
     public float gravityAcceleration;
     public float Speed;
+
+    public Vector2 direction { get; set; }
+
+    private Vector2 vDirection;
+    private Vector2 hDirection;
+
+    private float gravitySpeed;
+    private float distance;
     
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
+    
+    private void Move()
+    {
+
+    }
+    
 }
--- Assets/Scripts/Toolkit.cs
+++ Assets/Scripts/Toolkit.cs
@@ -36,4 +36,34 @@ public class Toolkit : MonoBehaviour {
         hitPoints = hitObjects;
         return hit;
     }
+
+    public static bool CheckMoveFloat(Vector2 originPosition, Vector2 vecSize, Vector2 direction, float distance, int layerNumber, out List<RaycastHit2D> hitPoints)
+    {
+        float threshold = 0.01f;
+        bool hit = false;
+        List<RaycastHit2D> hitObjects = new List<RaycastHit2D>();
+        Vector2 rayOrigin = originPosition + vecSize * (direction.x + direction.y) / 2;
+        Vector2 multiplier = Transpose2(direction);
+        float loopSize = Mathf.Abs(direction.x) * vecSize.y + Mathf.Abs(direction.y) * vecSize.x;
+        for (int i = 0; i <= loopSize; i++)
+        {
+            float k = 0;
+            // first point threshold
+            if (i == 0)
+                k = threshold;
+            // last point threshold
+            if (i == loopSize)
+                k = -threshold;
+            RaycastHit2D hitPoint = Physics2D.Raycast(rayOrigin - multiplier * (i + k), direction, distance, layerNumber, 0, 0);
+            if (hitPoint.collider != null)
+            {
+                hit = true;
+                hitObjects.Add(hitPoint);
+            }
+        }
+        hitObjects.Sort(new HitDistanceCompare());
+        hitPoints = hitObjects;
+        return hit;
+    }
+
 }
