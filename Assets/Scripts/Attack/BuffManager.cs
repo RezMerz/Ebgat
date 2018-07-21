7b43c8308e9d473079a8a4a693d6f93e6f3ecf92
Noise <rmirzaee95@gmail.com>
2018-07-21 15:23:41 +0430

    Buff Created and Coded

---
+++ Assets/Scripts/Attack/BuffManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuffManager : MonoBehaviour {
+    private CharacterAttributes charStats;
+    public Buff[] buffListRefrence;
+
+    void Start()
+    {
+        charStats = GetComponent<CharacterAttributes>();
+    }
+    public void ActivateBuff(string name)
+    {
+        foreach(Buff buff in buffListRefrence)
+            if (buff.name == name)
+            {
+                Buff thisBuff = Instantiate(buff);
+                thisBuff.charStats = charStats;
+            }
+    }
+}
