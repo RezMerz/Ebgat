4b46e68d0c8569a567311a8c5b4a90261c87122a
Yashar Fallah <Yashar.fallah1372@gmail.com>
2018-08-04 18:51:28 +0430

    offline character

---
+++ Assets/Scripts/Character/Offline/OfflineCharacterGravity.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OfflineCharacterGravity : MonoBehaviour {
+
+    private CharacterAttributes charStats;
+    private List<RaycastHit2D> hitObjects;
+    private Transform transformHero;
+    private float timer;
+    private int mask;
+
+
+    // Use this for initialization
+    private void Start()
+    {
+        mask = LayerMask.GetMask("Blocks", "Bridge");
+        transformHero = transform;
+        hitObjects = new List<RaycastHit2D>();
+        charStats = GetComponent<CharacterAttributes>();
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+        SpeedCheck();
+        HeroGravity();
+    }
+    private void HeroGravity()
+    {
+        // not int jumping state
+        if (charStats.FeetState != EFeetState.Jumping)
+        {
+            bool hit = Toolkit.CheckMove(transformHero.position, charStats.size, Vector2.down, Time.deltaTime * charStats.gravitySpeed, mask, out hitObjects);
+
+            if (charStats.FeetState == EFeetState.Onground)
+            {
+                // Go to Falling
+                if (!hit)
+                {
+                    if (timer >= charStats.cayoteTime)
+                    {
+                        charStats.FeetState = EFeetState.Falling;
+                        transformHero.position += Vector3.down * (Time.deltaTime * charStats.gravitySpeed);
+                        timer = 0;
+                    }
+                    else
+                    {
+                        timer += Time.deltaTime;
+                    }
+                }
+
+            }
+            else if (charStats.FeetState == EFeetState.Falling)
+            {
+                // Go to on Ground state
+                if (hit)
+                {
+                    var hitObject = hitObjects[0];
+
+                    if(hitObject.collider.gameObject.layer != LayerMask.NameToLayer("Bridge"))
+                    {
+                        transformHero.position += Vector3.down * (hitObject.distance);
+                        charStats.FeetState = EFeetState.Onground;
+                    }
+                    else
+                    {
+                        if(hitObject.point.y == hitObject.collider.transform.position.y + hitObject.collider.transform.localScale.y / 2)
+                        {
+                            transformHero.position += Vector3.down * (hitObject.distance);
+                            charStats.FeetState = EFeetState.Onground;
+                        }
+                        else
+                        {
+                            transformHero.position += Vector3.down * (Time.deltaTime * charStats.gravitySpeed);
+                        }
+                    }
+                }
+                // Still Faliing
+                else
+                {
+                    transformHero.position += Vector3.down * (Time.deltaTime * charStats.gravitySpeed);
+                }
+            }
+        }
+    }
+    private void SpeedCheck()
+    {
+        // if falling increase speed of gravity
+        if (charStats.FeetState == EFeetState.Falling)
+        {
+            charStats.gravitySpeed += charStats.gravityAcceleration * Time.deltaTime;
+            if (charStats.gravitySpeed > charStats.gravitySpeedMax)
+            {
+                charStats.gravitySpeed = charStats.gravitySpeedMax;
+            }
+        }
+        // if onground reset speed o gravity
+        if (charStats.FeetState == EFeetState.Onground)
+        {
+            charStats.ResetGravitySpeed();
+        }
+    }
+
+    public void  ChangeMask(int masknumber)
+    {
+        timer = charStats.cayoteTime;
+        mask = masknumber;
+    }
+}
