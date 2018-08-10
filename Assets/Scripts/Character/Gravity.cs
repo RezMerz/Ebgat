06f7509b9552b2d037b0fa821a800aae2ee9eb6f
Aref <valipour.aref@gmail.com>
2018-08-10 16:11:36 +0430

    gravity

--- Assets/Scripts/Character/Gravity.cs
+++ Assets/Scripts/Character/Gravity.cs
@@ -48,7 +48,6 @@ public class Gravity : MonoBehaviour {
 
     private void HitFunction(List<RaycastHit2D> vHits, List<RaycastHit2D> hHits, Vector2 direction)
     {
-        Debug.Log(timer);
         if (vHits.Count > 0 && direction.y < 0)
         {
             timer = 0;
