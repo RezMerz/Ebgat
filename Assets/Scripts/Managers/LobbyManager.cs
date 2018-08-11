78d54b58c5a8c6d4c3aea243def3ce6ed8aa23c2
Aref <valipour.aref@gmail.com>
2018-08-11 18:22:33 +0430

    network discovery

--- Assets/Scripts/Managers/LobbyManager.cs
+++ Assets/Scripts/Managers/LobbyManager.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class LobbyManager : MonoBehaviour {
+    public CustomNetworkDiscovery networkDiscovery;
 
 	// Use this for initialization
 	void Start () {
-		
+        if (networkDiscovery == null)
+            Debug.Log("network discovery is null");
+        else
+            networkDiscovery.action += OnHostFound;
 	}
 	
 	// Update is called once per frame
@@ -15,6 +20,14 @@ public class LobbyManager : MonoBehaviour {
 	}
 
     public void RefreshButtonPressed(){
-        Debug.Log("here");
+        networkDiscovery.StartAsClient();
+    }
+
+    public void HostButtonPressed(){
+        networkDiscovery.StartAsServer();
+    }
+
+    public void OnHostFound(string fromAddress, string data){
+        Debug.Log("found");
     }
 }
---
+++ Assets/Scripts/Network/CustomNetworkDiscovery.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System;
+
+public class CustomNetworkDiscovery : NetworkDiscovery {
+
+    public Action<string, string> action;
+
+    void Awake()
+    {
+        Initialize();
+    }
+
+    public override void OnReceivedBroadcast(string fromAddress, string data)
+    {
+        if(action != null){
+            action(fromAddress, data);
+        }
+    }
+}
