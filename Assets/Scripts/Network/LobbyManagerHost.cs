ff39c6123ee228f04ed23714958cb8aad8ee477c
Aref <valipour.aref@gmail.com>
2018-08-12 16:58:51 +0430

    new network hud

---
+++ Assets/Scripts/Network/LobbyManagerHost.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LobbyManagerHost : MonoBehaviour {
+
+    public NetworkDiscovery networkDiscovery;
+
+	// Use this for initialization
+	void Start () {
+        if (networkDiscovery == null)
+            Debug.Log("no network discovery");
+        else
+        {
+            networkDiscovery.Initialize();
+            networkDiscovery.StartAsServer();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
