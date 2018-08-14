1191e576fe631bece3ff21991488716df876484d
Noise <rmirzaee95@gmail.com>
2018-08-14 22:14:58 +0430

    Print test

--- Assets/Scripts/Character/CharacterAttributes.cs
+++ Assets/Scripts/Character/CharacterAttributes.cs
@@ -384,6 +384,8 @@ public class CharacterAttributes : MonoBehaviour {
     }
     private void RegisterFeetState()
     {
+        print(playerControl);
+        priint(playerControl.worldState);
         switch (FeetState)
         {
             case EFeetState.Onground: playerControl.worldState.RegisterCharStat(ID, 'd', "1"); break;
