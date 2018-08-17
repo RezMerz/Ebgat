a61b45a4f439d885491803604fb5e1dc39f9c181
Yashar Fallah <Yashar.fallah1372@gmail.com>
2018-08-17 18:28:59 +0430

    fix

--- Assets/Scripts/Character/PlayerControl.cs
+++ Assets/Scripts/Character/PlayerControl.cs
@@ -78,7 +78,6 @@ public class PlayerControl : MonoBehaviour
     {
         //worldState.print();
         //Debug.Log(playerConnection + "   " + playerConnection.clientId + "   " + gameObject.GetInstanceID());
-        counter++;
         ReadData();
     }
 
