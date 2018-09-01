ad6b4f3041f22f3814f0f55075a6fa698091c9d5
Noise <rmirzaee95@gmail.com>
2018-09-02 00:50:43 +0430

    Bahram ro Bebandim

--- Assets/Scripts/Character/Client Side/CharacterAttributesClient.cs
+++ Assets/Scripts/Character/Client Side/CharacterAttributesClient.cs
@@ -18,4 +18,36 @@ public class CharacterAttributesClient : MonoBehaviour {
     public void SetReady(){
       
     }
+
+    public void SetAttribute(char attributeCode, string value)
+    {
+        switch (attributeCode)
+        {
+         //   case 'a': SetHeadState(value); break;
+          //  case 'b': SetBodyState(value); break;
+        //    case 'c': SetHandState(value); break;
+       //     case 'd': SetFeetState(value); break;
+            case 'e': side = Toolkit.DeserializeVector(value); break;
+        //    case 'f': armor = float.Parse(value, CultureInfo.InvariantCulture.NumberFormat); break;
+       //     case 'g': hitPoints = float.Parse(value, CultureInfo.InvariantCulture.NumberFormat); break;
+       //     case 'h': SetAttackMode(value); break;
+       //     case 'i': range = float.Parse(value, CultureInfo.InvariantCulture.NumberFormat); break;
+       //     case 'j': attackDamage = float.Parse(value, CultureInfo.InvariantCulture.NumberFormat); break;
+       //     case 'k': attackAnimationTime = float.Parse(value, CultureInfo.InvariantCulture.NumberFormat); break;
+      //      case 'l': attackCooldown = float.Parse(value, CultureInfo.InvariantCulture.NumberFormat); break;
+      //      case 'm': moveSpeed = float.Parse(value, CultureInfo.InvariantCulture.NumberFormat); break;
+       //     case 'n': moveAcceleration = float.Parse(value, CultureInfo.InvariantCulture.NumberFormat); break;
+        //    case 'o': moveSpeedMax = float.Parse(value, CultureInfo.InvariantCulture.NumberFormat); break;
+       //     case 'p': jumpSpeed = float.Parse(value, CultureInfo.InvariantCulture.NumberFormat); break;
+       //     case 'q': jumpAcceleration = float.Parse(value, CultureInfo.InvariantCulture.NumberFormat); break;
+       //     case 'r': jumpSpeedMax = float.Parse(value, CultureInfo.InvariantCulture.NumberFormat); break;
+       //     case 's': cayoteTime = float.Parse(value, CultureInfo.InvariantCulture.NumberFormat); break;
+       //     case 't': gravitySpeed = float.Parse(value, CultureInfo.InvariantCulture.NumberFormat); break;
+       //     case 'u': gravityAcceleration = float.Parse(value, CultureInfo.InvariantCulture.NumberFormat); break;
+        //    case 'v': gravitySpeedMax = float.Parse(value, CultureInfo.InvariantCulture.NumberFormat); break;
+        //    case 'w': aimSide = Toolkit.DeserializeVector(value); break;
+        //    case 'x': energy = int.Parse(value); break;
+        //    case 'A': SetAbilityState(value); break;
+        }
+    }
 }
