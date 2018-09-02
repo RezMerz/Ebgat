ab5aa88ede083597254ff7d68591c0247608658c
Yashar Fallah <Yashar.fallah1372@gmail.com>
2018-09-02 18:30:36 +0430

    PlayerControl

--- Assets/Scripts/Character/PlayerControl.cs
+++ Assets/Scripts/Character/PlayerControl.cs
@@ -154,7 +154,7 @@ public class PlayerControl : MonoBehaviour
                 if (IsLocalPlayer())
                 {
                     waitingForRequest = true;
-                   // clientNetworkSender.RequestWorldState(playerId);
+                    clientNetworkSender.RequestWorldState(playerId);
                 }
             }
             currentStateNumber++;
@@ -404,7 +404,7 @@ public class PlayerControl : MonoBehaviour
     {
         if (IsLocalPlayer())
         {
-            Debug.Log(id + "+" + Time.frameCount);
+          //  Debug.Log(id + "+" + Time.frameCount);
         }
         if (!start && (!firstRecieved || currentStateNumber <= id))
         {
