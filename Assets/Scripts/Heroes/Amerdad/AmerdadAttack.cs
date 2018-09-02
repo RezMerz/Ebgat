1fec4cacc0ac03e93b647b0f4cf1f7c6c7e70c8f
asd <>
2018-09-03 00:05:10 +0430

    asd

--- Assets/Scripts/Heroes/Amerdad/AmerdadAttack.cs
+++ Assets/Scripts/Heroes/Amerdad/AmerdadAttack.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AmerdadAttack : MonoBehaviour {
+public class AmerdadAttack : Attack {
 
 	// Use this for initialization
 	void Start () {
