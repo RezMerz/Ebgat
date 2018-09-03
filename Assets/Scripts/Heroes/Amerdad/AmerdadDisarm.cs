f2a46c66d8634fd6a4349b00d90fa0a54105a33b
asd <>
2018-09-03 14:23:17 +0430

    amerdad

---
+++ Assets/Scripts/Heroes/Amerdad/AmerdadDisarm.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmerdadDisarm : Ability {
+    public override void AbilityActivateClientSide()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void AbilityKeyHold()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void AbilityKeyPrssed()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void AbilityKeyReleased()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
