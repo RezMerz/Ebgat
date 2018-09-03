18d20abb8589c3dc5c2bbfc55cc958ab9deceb03
Yashar Fallah <Yashar.fallah1372@gmail.com>
2018-09-03 16:43:55 +0430

    small fix

--- Assets/Scripts/BulletManager.cs
+++ Assets/Scripts/BulletManager.cs
@@ -2,46 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletManager : MonoBehaviour {
+public class BulletManager : MonoBehaviour
+{
     [SerializeField]
     private GameObject[] bulletObjects;
     private Hashtable shotBullets = new Hashtable();
     private int layerMask;
-    private PlayerControl playerControl;
+    private PlayerControlClientside playerControl;
 
     private void Start()
     {
-        playerControl = GetComponent<PlayerControl>();
+        playerControl = GetComponent<PlayerControlClientside>();
         playerControl.ReadyAction += Initialize;
     }
     private void Initialize()
     {
-        if (playerControl.IsServer())
-        {
-            layerMask = LayerMask.GetMask(playerControl.charStats.enemyTeamName, "Blocks");
-        }
-        else
-        {
-            layerMask = LayerMask.GetMask(playerControl.charStatsClient.enemyTeamName, "Blocks");
-        }
+        layerMask = LayerMask.GetMask(playerControl.charStatsClient.enemyTeamName, "Blocks");
     }
 
-    public void Shoot(Vector2 direction,float gravityAcc, int id,float range,int number,Vector2 startPos)
+    public void Shoot(Vector2 direction, float gravityAcc, int id, float range, int number, Vector2 startPos)
     {
-        Vector2 side;
-        if (playerControl.IsServer())
-        {
-            side = playerControl.charStats.Side;
-        }
-        else
-        {
-            side = playerControl.charStatsClient.side;
-        }
         GameObject bulletObject = Instantiate(bulletObjects[number], transform.position + (Vector3)startPos, Quaternion.identity);
         bulletObject.layer = gameObject.layer;
         shotBullets.Add(id, bulletObject);
-        bulletObject.GetComponent<Bullet>().Shoot(direction,layerMask, gravityAcc,range);
+        bulletObject.GetComponent<Bullet>().Shoot(direction, layerMask, gravityAcc, range);
     }
+
     public void DestroyBullet(int id)
     {
         if (shotBullets.Contains(id))
