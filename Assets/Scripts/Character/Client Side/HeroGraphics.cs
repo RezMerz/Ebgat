d1963c8c5acb4b0c560dd50bc212307b1c1edf65
Noise <rmirzaee95@gmail.com>
2018-09-06 21:36:46 +0430

    Malkous ulti Test

--- Assets/Scripts/Character/Client Side/HeroGraphics.cs
+++ Assets/Scripts/Character/Client Side/HeroGraphics.cs
@@ -58,7 +58,11 @@ public class HeroGraphics : MonoBehaviour
         else if (value == "False")
         {
             if (rootGround != null)
+            {
                 rootGround.GetComponent<Animator>().SetTrigger("UnRoot");
+                DestoryObjectAfterTime(3, rootGround);
+                rootGround = null;
+            }
         }
     }
 
