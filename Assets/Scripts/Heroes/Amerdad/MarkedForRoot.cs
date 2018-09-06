08699cf3fb639935bc33d4c3d9f6a1882f00518b
Yashar Fallah <Yashar.fallah1372@gmail.com>
2018-09-06 16:49:32 +0430

    kkk

---
+++ Assets/Scripts/Heroes/Amerdad/MarkedForRoot.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarkedForRoot : Buff {
+
+    public float damage;
+    public  Buff rootBuff;
+
+    private Gravity gravity;
+    private PlayerControl playerControl;
+
+    
+    public override void BuffCharacter()
+    {
+        charStats.MarkedForRoot = true;
+        gravity.GravityAction += Root;
+    }
+
+    public override void DebuffCharacter()
+    {
+        charStats.MarkedForRoot = false;
+        gravity.GravityAction += Root;
+    }
+
+    private void Root()
+    {
+        playerControl.TakeAttack(damage, rootBuff.name);
+    }
+
+    void Start ()
+    {
+        gravity = GetComponent<Gravity>();
+        playerControl = GetComponent<PlayerControl>();
+	}
+	
+
+}
