88ae164ad1d064fec7f304a68e84dbc9c7498f49
Noise <rmirzaee95@gmail.com>
2018-09-07 22:29:21 +0430

    Sliders Added

--- Assets/Scripts/UI/Music.cs
+++ Assets/Scripts/UI/Music.cs
@@ -3,5 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Music : MonoBehaviour {
+    public static Music instance; 
+    void Start()
+    {
+        if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "Arena 2")
+            Destroy(Music.instance);
 
+        if (instance == null)
+            instance = null;
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
 }
